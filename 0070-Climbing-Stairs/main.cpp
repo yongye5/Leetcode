4fdefe03c8c96721fc4868c0d97c22dbeb585501
zhangpeng <1970187812@qq.com>
2019-10-30 15:16:27 +0800

    70

---
+++ 0070-Climbing-Stairs/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution{
+private:
+vector<int> vec;
+    int calcWays(int n){
+        if(n == 1 ){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        if(vec[n] == -1){
+            vec[n] = calcWays(n - 1) + calcWays(n - 2);
+        }
+        return vec[n];
+    }
+public:
+    int climbStairs(int n){
+        vec = vector<int>(n + 1, -1);
+        return calcWays(n);
+    }
+    // 打印记录的vec值
+    void print(){
+        for(int i = 0; i < vec.size(); i++){
+            cout << vec[i] << " ";
+        }
+    }
+};
+// 运用记忆化搜索的方法
+int main(){
+    Solution s;
+    int value = s.climbStairs(10);
+    cout << "value = " << value << endl;
+    s.print();
+    system("pause");
+    return 0;
+}
