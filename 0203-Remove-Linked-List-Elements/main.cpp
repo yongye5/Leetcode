fc63e4d1c4ed682d9f51627598f3686de8856ebb
zhangpeng <1970187812@qq.com>
2019-11-06 15:46:33 +0800

    dummyNode

---
+++ 0203-Remove-Linked-List-Elements/main.cpp
@@ -0,0 +1,62 @@
+/**
+ * @description: https://leetcode-cn.com/problems/remove-linked-list-elements/submissions/
+ * @author: zp
+ * @date:2019-11-06 15:43:14
+ **/
+
+#include <iostream>
+
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x), next(NULL){}
+};
+ListNode* createLinkedList(int arr[], int n){
+    if(n == 0){
+        return NULL;
+    }
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curNode = head;
+    for(int i = 1; i < n; i ++){
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+    return head;
+}
+void printLinkedList(ListNode* head){
+    ListNode* curNode = head;
+    while(curNode != NULL){
+        cout <<curNode->val << " -> ";
+        curNode = curNode->next;
+    }
+    cout << "NULL" << endl;
+}
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyNode = new ListNode(0);
+        dummyNode->next = head;
+        ListNode* cur = dummyNode;
+        while(cur->next != NULL){
+            if(cur->next->val == val){
+                cur->next = cur->next->next;
+            }else{
+                cur = cur->next;
+            }
+        }
+        
+        ListNode* retNode = dummyNode->next;
+        delete dummyNode;
+        return  retNode;
+    }
+};
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    ListNode *list = createLinkedList(arr, n);
+    printLinkedList(Solution().removeElements(list, 1));
+    system("pause");
+    return 0;
+}
